5d4f16428ad3af68d967515ff593cf6e730d9545
Yoshihara-Yuji-Fukuyama <gc23002@anabuki-net.ne.jp>
2023-09-21 12:13:41 +0900

    課題2

---
+++ 3DLv1_vs2019_00/GameProgramming/src/CVector.cpp
@@ -0,0 +1,24 @@
+#include "CVector.h"
+
+//Set(XÀ•W,YÀ•W,ZÀ•W)
+void CVector::Set(float x, float y, float z)
+{
+	mX = x;
+	mY = y;
+	mZ = z;
+}
+
+float CVector::X() const
+{
+	return mX;
+}
+
+float CVector::Y() const
+{
+	return mY;
+}
+
+float CVector::Z() const
+{
+	return mZ;
+}
