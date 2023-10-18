f6b0257c76d33f43d6a9b08f289d3a0160f2ae44
Yoshihara-Yuji-Fukuyama <gc23002@anabuki-net.ne.jp>
2023-10-18 10:39:38 +0900

    課題13

--- 3DLv1_vs2019_00/GameProgramming/src/CMatrix.cpp
+++ 3DLv1_vs2019_00/GameProgramming/src/CMatrix.cpp
@@ -1,6 +1,10 @@
 #include "CMatrix.h"
 //標準入出力関数のインクルード
 #include <stdio.h>
+//円周率M_PIを有効にする
+#define _USE_MATH_DEFINES
+//数学関数のインクルード
+#include <math.h>
 
 void CMatrix::Print()
 {
@@ -45,4 +49,52 @@ CMatrix CMatrix::Scale(float sx, float sy, float sz)
 float CMatrix::M(int r, int c)const
 {
 	return mM[r][c];
+}
+
+//回転行列(Y軸)の作成
+//RotateY(角度)
+CMatrix CMatrix::RotateY(float degree)
+{
+	//角度からラジアンを求める
+	float rad = degree / 180.0f * M_PI;
+	//単位行列にする
+	Identity();
+	//Y軸で回転する行列の設定
+	mM[0][0] = mM[2][2] = cosf(rad);
+	mM[0][2] = -sinf(rad);
+	mM[2][0] = -mM[0][2];
+	//行列を返す
+	return *this;
+}
+
+//回転行列(Z軸)の作成
+//RotateZ(角度)
+CMatrix CMatrix::RotateZ(float degree)
+{
+	//角度からラジアンを求める
+	float rad = degree / 180.0f * M_PI;
+	//単位行列にする
+	Identity();
+	//Z軸で回転する行列の設定
+	mM[0][0] = mM[1][1] = cosf(rad);
+	mM[0][1] = sinf(rad);
+	mM[1][0] = -mM[0][1];
+	//行列を返す
+	return *this;
+}
+
+//回転行列(X軸)の作成
+//RotateX(角度)
+CMatrix CMatrix::RotateX(float degree)
+{
+	//角度からラジアンを求める
+	float rad = degree / 180.0f * M_PI;
+	//単位行列にする
+	Identity();
+	//X軸で回転する行列の設定
+	mM[1][1] = mM[2][2] = cosf(rad);
+	mM[1][2] = sinf(rad);
+	mM[2][1] = -mM[1][2];
+	//行列を返す
+	return *this;
 }
