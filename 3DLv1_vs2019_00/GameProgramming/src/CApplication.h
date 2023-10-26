db6431c5413e232f7dd903211e28043dd48a3b69
Yoshihara-Yuji-Fukuyama <gc23002@anabuki-net.ne.jp>
2023-10-26 12:03:49 +0900

    課題17

--- 3DLv1_vs2019_00/GameProgramming/src/CApplication.h
+++ 3DLv1_vs2019_00/GameProgramming/src/CApplication.h
@@ -23,7 +23,7 @@
 class CApplication
 {
 private:
-	CCharacter3 mPlayer;
+	CPlayer mPlayer;
 	CCharacter3 mCharacter;
 	CModel mBackGround;//背景モデル
 	CModel mModel; //モデルクラスのインスタンス作成
--- 3DLv1_vs2019_00/GameProgramming/src/CPlayer.cpp
+++ 3DLv1_vs2019_00/GameProgramming/src/CPlayer.cpp
@@ -1,73 +1,39 @@
+//プレイヤークラスのインクルード
 #include "CPlayer.h"
-#include "CApplication.h"
-//#include "CGame.h"
 
-#define TEXCOORD 168, 188, 158, 128	//テクスチャマッピング
-#define GRAVITY (TIPSIZE / 20.0f)	//重力加速度
-#define JUMPV0 (TIPSIZE / 1.4f)		//ジャンプの初速
+#define ROTATION_YV CVector(0.0f,1.0f,0.0f)//回転速度
+#define VELOCITY CVector(0.0f,0.0f,0.1f)//移動速度
 
-CPlayer::CPlayer(float x, float y, float w, float h, CTexture* pt)
-	: mVy(0.0f)
+//CPlayer(位置,回転,スケール)
+CPlayer::CPlayer(const CVector& pos, const CVector& rot, const CVector& scale)
 {
-	Set(x, y, w, h);
-	Texture(pt, TEXCOORD);
-	mTag = ETag::EPLAYER;
+	CTransform::Update(pos, rot, scale);//行列の更新
 }
 
+//更新処理
 void CPlayer::Update()
 {
-	if (mInput.Key(VK_SPACE))
-	{
-		CApplication::CharacterManager()->Add(
-			new CBullet(X(), Y() + H() + 10.0f
-				, 3.0f, 10.0f, 1396, 1420, 750, 592, CApplication::Texture()));
-	}
+	//Aキー入力で回転
 	if (mInput.Key('A'))
 	{
-		float x = X() - 4.0f;
-		X(x);
+		//Y軸の回転値を増加
+		mRotation = mRotation + ROTATION_YV;
 	}
 
+	//Dキー入力で回転
 	if (mInput.Key('D'))
 	{
-		float x = X() + 4.0f;
-		X(x);
+		//Y軸の回転値を減少
+		mRotation = mRotation - ROTATION_YV;
 	}
 
-	if (mInput.Key('J') 
-		&& mState != EState::EJUMP
-		&& mVy >= 0.0f)
+	//上キー入力で前進
+	if (mInput.Key(VK_UP))
 	{
-		mVy = JUMPV0;
-		mState = EState::EJUMP;
+		//Z軸方向の値を回転させ移動させる
+		mPosition = mPosition + VELOCITY * mMatrixRotate;
 	}
 
-	Y(Y() + mVy);
-	mVy -= GRAVITY;
-}
-
-void CPlayer::Collision()
-{
-	CApplication::CharacterManager()->Collision(this);
-}
-
-void CPlayer::Collision(CCharacter* m, CCharacter* o)
-{
-	float x, y;
-	switch (o->Tag())
-	{
-	case ETag::EPLAYER:
-		break;
-	default:
-		if (CRectangle::Collision(o, &x, &y))
-		{
-			X(X() + x);
-			Y(Y() + y);
-			if (x == 0.0f)
-			{
-				mVy = 0.0f;
-				mState = EState::EMOVE;
-			}
-		}
-	}
+	//変換行列の更新
+	CTransform::Update();
 }
