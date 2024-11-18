3970b2e70a72fc5cb3dbef21ef853c1e17f9f439
Yoshihara-Yuji-Fukuyama <gc23002@anabuki-net.ne.jp>
2024-11-18 16:57:19 +0900

    日報1118

--- 2年生後期制作/project/借金(仮)/Debt/src/Game/CCarAndTruckManager.h
+++ 2年生後期制作/project/借金(仮)/Debt/src/Game/CCarAndTruckManager.h
@@ -12,6 +12,8 @@ public:
 	void Update();
 	// 使用する車とトラックを全て生成
 	void CreateCarAndTruck(CModel* car, CModel* garbageTruck);
+	// ステージ範囲外の車、トラックの更新、描画を止める
+	void HideCarAndTruck();
 private:
 	std::vector<CCar*> mCars;	// 車のリスト
 	CGarbageTruck* mpGarbageTruck;// 通常トラック
